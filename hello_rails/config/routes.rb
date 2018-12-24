ac0ea3053b7289b58edc27b5d78aeb6de255c6a4
PenutChen <PenutChen@PenutLaptop>
2018-12-24 13:10:47 +0800

    Some Test

--- hello_rails/config/routes.rb
+++ hello_rails/config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get "welcome/say_hello" => "welcome#say"
   get "welcome" => "welcome#index"
   root :to => "welcome#index"
+  match ':controller(/:action(/:id(.:format)))', :via => :all
 end
