91dea301894298451166815e00b30daa03f62ec6
PenutChen <Penut@PenutLab>
2018-12-27 11:01:11 +0800

    Update

---
+++ ror.sh
@@ -0,0 +1,7 @@
+sudo apt-get install ruby-dev -y
+sudo apt-get install zlib1g-dev -y
+sudo apt-get install libsqlite3-dev -y
+sudo gem install sqlite3 -v '1.3.13' --source 'https://rubygems.org/' -y
+sudo apt-get install nodejs -y
+sudo gem install rails -y
+echo hello
